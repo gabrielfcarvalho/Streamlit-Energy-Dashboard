3d0ecffd5d00727abffd9b56b6be8ccf6ca41e51
Gabriel Fernandes Carvalho <32232104+gabrielfcarvalho@users.noreply.github.com>
2023-12-01 15:12:07 -0300

    Update dashboard_cemig.py

--- dashboard_cemig.py
+++ dashboard_cemig.py
@@ -137,7 +137,7 @@ def display_suggested_energy_distribution(data, selected_month):
         st.write("Não há dados de consumo para exibir.")
 
 # Seletor de páginas na barra lateral
-page = st.sidebar.radio("### O que você gostaria de analisar?", ("Métricas", "Gráficos", "Distribuição de Energia e Sugestão"))
+page = st.sidebar.radio("### **O que você gostaria de analisar?**", ("Métricas", "Gráficos", "Distribuição de Energia e Sugestão"))
 
 # Exibindo a página selecionada
 if page == "Métricas":
