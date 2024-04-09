5f3f82c2617a90f2cdfbd40406f105adde046111
Gabriel Fernandes Carvalho <32232104+gabrielfcarvalho@users.noreply.github.com>
2024-04-09 10:51:46 -0300

    Update dashboard_cemig.py

--- dashboard_cemig.py
+++ dashboard_cemig.py
@@ -142,8 +142,6 @@ def show_metrics_page():
     with st.expander("Consumo Total por Localidade", expanded=False):
         consumo_por_localidade = metrics['Consumo por Localidade']
         total_consumo_geral = sum(consumo_por_localidade.values())  # Calcula o total de consumo geral para o período
-        for loc, consumo in consumo_por_localidade.items():
-            st.metric(label=f"{loc}", value=f"{consumo:.2f} kWh")
         if total_consumo_geral > 0:
             for loc, consumo in consumo_por_localidade.items():
                 proporcao = (consumo / total_consumo_geral) * 100  # Calcula a proporção
